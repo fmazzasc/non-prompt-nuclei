6c595adc8f7bb83ca5c59affc4c17404e7337cd7
Francesco Mazzaschi <fmazzasc@alipap1.cern.ch>
2025-03-17 14:23:32 +0100

    Add downloader

---
+++ download.sh
@@ -0,0 +1,5 @@
+mkdir -p data
+mkdir -p mc
+alien_cp alien:/alice/cern.ch/user/a/alihyperloop/outputs/0036/367418/88263/AO2D.root file:data/AO2D_an.root
+alien_cp alien:/alice/cern.ch/user/a/alihyperloop/outputs/0029/290850/48265/AO2D.root file:data/AO2D_al.root
+alien_cp alien:/alice/cern.ch/user/a/alihyperloop/outputs/0033/337353/73325/AO2D.root file:mc/AO2D_25a3.root
